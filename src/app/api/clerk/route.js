0ee3cc52fc2d77f33cd31b4e61a64e0b63594394
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-08-12 12:54:27 +0530

    implement user management API with webhook verification and MongoDB integration

---
+++ src/app/api/clerk/route.js
@@ -0,0 +1,51 @@
+import connectionDB from "@/config/db.config";
+import UserModel from "@/models/User.model";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+
+
+
+export default async function POST(req, content) {
+    
+    const wh = new Webhook(process.env.SIGNIN_SECRET)
+    const headerpayload = await headers()
+    const svixHeaders = {
+        'svix-id': headerpayload.get('svix-id'),
+        'svix-timestamp': headerpayload.get('svix-timestamp'),
+        'svix-signature': headerpayload.get('svix-signature'),
+    }
+
+    //get the payload and verify it
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const { data, type } = wh.verify(body, svixHeaders)
+
+    //prepare the user data to save in the database
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    }
+
+    await connectionDB()
+
+    switch (value) {
+        case 'user.created':
+            await UserModel.create(userData)
+            break;
+        case 'user.updated':
+            await UserModel.findByIdAndUpdate(data.id , userData)
+            break;
+        case 'user.deleted':
+            await UserModel.findByIdAndDelete(data.id)
+            break;
+
+        default:
+            break;
+    }
+
+    return NextResponse.json({success:true, message : 'Message received'})
+
+}
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 
 
-let connection = async () => {
+let connectionDB = async () => {
     try {
         let connectionInstence = await mongoose.connect(`${process.env.mongoDB_url}`)
 
         console.log(`\n MongoDB is connected Successfully | DB_host = ${connectionInstence.connection.host} | DB_port = ${connectionInstence.connection.port} | DB_name = ${connectionInstence.connection.name}\n`)
-        
+
     } catch (error) {
         console.error('MongoDB connection error -', error)
         process.exit(1);
     }
 }
 
-export default connection ;
+export default connectionDB ;
