bcf26251fdab62dfe4b5515fde34934635667ad2
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-08-12 11:43:40 +0530

    add Chat model schema for managing chat data

---
+++ src/models/Chat.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const ChatSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    message: [
+        {
+            role: { type: String, required: true },
+            content: { type: String, required: true },
+            timestamps: { type: Number, required: true }
+        }
+    ],
+    userId: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true })
+
+export default ChatModel = mongoose.models.ChatModel || mongoose.model('Chat', ChatSchema)
