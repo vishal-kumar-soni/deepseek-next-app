9c5136638da07b6c363739d479b03a35de36476e
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-08-13 22:55:15 +0530

    implement POST route for chat creation

---
+++ src/app/api/chat/create/route.js
@@ -0,0 +1,41 @@
+import connectionDB from "@/config/db.config";
+import ChatModel from "@/models/Chat.model";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req);
+
+        if (!userId) {
+            return NextResponse.json({
+                success: false,
+                message: 'User not authenticated'
+            })
+        }
+
+        // Prepare the chat data to be saved in the database 
+        let chatdata = {
+            userId,
+            message: [],
+            name: 'New Chat',
+        }
+
+        //Connect the database and create a new chat
+        await connectionDB()
+        await ChatModel.create(chatdata);
+
+        return NextResponse.json({
+            success: true,
+            message: 'Chat is created'
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: error?.message,
+        })
+    }
+
+}
--- src/models/Chat.model.js
+++ src/models/Chat.model.js
@@ -1,25 +1,26 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const ChatSchema = new mongoose.Schema({
-
-    name: {
-        type: String,
-        required: true
+        userId: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        message: [
+            {
+                role: { type: String, required: true },
+                content: { type: String, required: true },
+                timestamps: { type: Number, required: true },
+            },
+        ],
     },
-    message: [
-        {
-            role: { type: String, required: true },
-            content: { type: String, required: true },
-            timestamps: { type: Number, required: true }
-        }
-    ],
-    userId: {
-        type: String,
-        required: true
-    },
+    { timestamps: true },
+);
 
-}, {timestamps:true })
+const ChatModel =
+    mongoose.models.ChatModel || mongoose.model("Chat", ChatSchema);
 
-const ChatModel = mongoose.models.ChatModel || mongoose.model('Chat', ChatSchema)
-
-export default ChatModel
+export default ChatModel;
