63af3e1c7b9b26bf72ec963e37202672f16238c0
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-08-14 09:35:27 +0530

    add POST route for chat interaction with OpenAI API

---
+++ src/app/api/chat/ai/route.js
@@ -0,0 +1,70 @@
+export const maxDuration = 60;
+import connectionDB from "@/config/db.config";
+import ChatModel from "@/models/Chat.model";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+
+const openai = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: process.env.DEEPSEEK_API_KEY,
+});
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req)
+
+        // Extract chatId and request from the request body
+        const { chatId, prompt } = await req.json();
+
+        if (!userId) {
+            return NextResponse.json({
+                success: false,
+                message: 'User not authenticated'
+            })
+        }
+
+        // Find the chat document in the database based on chatId and userId
+        await connectionDB()
+        const data = await ChatModel.find({ userId, _id: chatId })
+
+        // Create a user message object
+        const userPrompt = {
+            role: "user",
+            content: prompt,
+            timestamp: Date.now()
+        }
+
+        data.message.push(userPrompt)
+
+        // Call the deepseek API to get a chat complition
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: prompt }],
+            model: "deepseek-v4-pro",
+            store: true,
+            thinking: { "type": "enabled" },
+            reasoning_effort: "high",
+            stream: false,
+        });
+
+        const message = completion.choices[0].message.content;
+        message.timestamp = date.now()
+        data.messages.push(message)
+        data.save()
+
+
+        return NextResponse.json({
+            success: true,
+            data,
+        })
+
+    } catch (error) {
+
+        return NextResponse.json({
+            success: false,
+            message: error.message,
+        })
+
+    }
+}
